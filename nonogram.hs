b78b4457bbe16189f05ab14e15db4c4090c5374a
João Alvim <joaoafonsoalvim@gmail.com>
2023-07-23 18:32:43 +0100

    common elements

--- nonogram.hs
+++ nonogram.hs
@@ -4,46 +4,49 @@ import Data.List
 
 --https://www.researchgate.net/figure/a-shows-an-example-of-a-Nonogram-Its-solution-is-shown-in-b-The-description-for_fig3_228862924
 
-type State = Bool
+data State = Fill | Cross | Unk deriving (Show, Eq)
 type Block = [Int]
 type Line  = [Block]
 
--- TIPO EMPTY? mudar o state
-
-fill,cross :: State
-fill  = True
-cross = False
-
--- (0,0) is the top left corner
 rows,collumns :: Line
 rows = [ [2,1],[1,3],[1,2],[3],[4],[1] ]
 collumns = [ [1],[5],[2],[5],[2,1],[2] ]
 
+crossPares []    = []
+crossPares [a]   = [a]
+crossPares (h:t) = (h ++ [Cross]) : crossPares t
+
+makeFill = crossPares . map (`replicate` Fill)
+
 delAll x l = [a | a <- l, a /= x]
 
-extra  = flip take $ repeat [cross]
+extra  = flip take $ repeat [Cross]
 
--- filtro para manter a mesma ordem relativa
--- usado apra gerar as permutações
 
+-- filtro para manter a mesma ordem relativa usado apra gerar as permutações
 --orel i = ( == i ) . delAll [cross]
-orel = (. delAll [cross]) . (==)
+orel = (. delAll [Cross]) . (==)
 
 
-perm n l = filter (orel l) $ permutations (pack n l) 
-
-pack n l = l ++ replicate x [cross]
+pack n l = l ++ replicate x [Cross]
     where x = n - (length $ concat l)
 
 
-crossPares []    = []
-crossPares [a]   = [a]
-crossPares (h:t) = (h ++ [cross]) : crossPares t
+-- all possibel line states
+perm n l = nub $ filter (orel l) $ permutations (pack n l) 
 
-makeFill = crossPares . map (`replicate` fill)
 
---inserir espaços a toa para gerar todas as cenas
+umequal (h:t) = all (h ==) t
 
--- all possibilities
---ap :: Line -> Int -> [Line]
-ap line n = makeFill line
+eqcomm (i,l)
+    | umequal l = (i,head l)
+    | otherwise = (i, Unk )
+
+
+commons = map eqcomm . zip [0..] . transpose . map concat
+
+--commons ll = map eqcomm simple
+--    where 
+--        simple = zip [0..] $ transpose $ concat ll
+
+gera n l = commons $ perm n $ makeFill l
